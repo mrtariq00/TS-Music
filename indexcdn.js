d7d41fa774222d1279118dca9c21d9235dc362a2
Mr_TARIQ <tariqbaloch697@gmail.com>
2025-09-25 19:33:59 +0500

    1.0.1 fix a bug

--- indexcdn.js
+++ indexcdn.js
@@ -1,5 +1,20 @@
 const API_URL = "https://raw.githubusercontent.com/mrtariq00/api-s/refs/heads/main/songsApi.json";
 
+async function getData() {
+  try {
+    const response = await fetch(API_URL);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data || data; // return songs array
+  } catch (error) {
+    console.error("Error fetching songs:", error);
+    return [];
+  }
+}
 async function getSongs() {
   try {
     const response = await fetch(API_URL);
@@ -9,14 +24,14 @@ async function getSongs() {
     }
 
     const data = await response.json();
-    return data; // songs ka pura data return karega
+    return data.songs || data; // return songs array
   } catch (error) {
     console.error("Error fetching songs:", error);
-    return null;
+    return [];
   }
 }
 
-// Example use:
-let TsMusic = await getSongs();
-
-console.log(TsMusic);
+const TsMusic = {
+  getSongs : await getSongs(),
+  getData : await getData()
+};
